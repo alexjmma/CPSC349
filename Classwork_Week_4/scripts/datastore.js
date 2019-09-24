e342f4c9f6137c0e4b0e86269feecc43865cdd60
Alex Ma <alexjmma@users.noreply.github.com>
2019-09-23 18:57:49 -0700

    finished chapter 8

--- Classwork_Week_4/scripts/datastore.js
+++ Classwork_Week_4/scripts/datastore.js
@@ -1,30 +1,31 @@
 // browser-sync start --server --browser "chrome.exe"  --files ".html, stylesheets/.css, scripts/*.js"
 // D:\Github\CPSC349\Classwork_Week_4
 (function(window) {
-  'use strict'
+  'use strict';
 
   //no code here
   var App = window.App || {};
 
   function DataStore() {
     console.log('running the DataStore function');
+    this.data = {};
   }
 
   DataStore.prototype.add = function(key, val) {
     this.data[key] = val;
-  }
+  };
 
-  DataStore.prototype.get = function(key, val) {
+  DataStore.prototype.get = function(key) {
     return this.data[key];
-  }
+  };
 
-  DataStore.prototype.getAll = function(key, val) {
+  DataStore.prototype.getAll = function() {
     return this.data;
-  }
+  };
 
-  DataStore.prototype.remove = function(key, val) {
+  DataStore.prototype.remove = function(key) {
     delete this.data[key];
-  }
+  };
 
 
   App.DataStore = DataStore;
